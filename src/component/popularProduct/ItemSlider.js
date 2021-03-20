f59900a1c6868fd0a4497f274ce55550a1cb1827
Moloy Pal <77716576+moloypal2021@users.noreply.github.com>
2021-03-21 00:27:42 +0600

    Update

--- src/component/popularProduct/ItemSlider.js
+++ src/component/popularProduct/ItemSlider.js
@@ -4,24 +4,18 @@ import Item from "./Item";
 
 export default class ItemSlider extends Component {
   render() {
-    const settings = {
-      dots: true,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-    };
     return (
       <div>
-        {/* <ScrollContainer className="scroll-container flex xl:space-x-14 lg:space-x-12 md:space-x-10 sm:space-x-8 space-x-5  w-full overflow-x-scroll hide-scrollbar"> */}
-
-        <Item />
-        <Item />
-        <Item />
-        <Item />
-        <Item />
-
-        {/* </ScrollContainer> */}
+        <ScrollContainer
+          vertical={false}
+          className="scroll-container flex xl:space-x-14 lg:space-x-12 md:space-x-10 sm:space-x-8 space-x-5  w-full"
+        >
+          <Item />
+          <Item />
+          <Item />
+          <Item />
+          <Item />
+        </ScrollContainer>
       </div>
     );
   }
